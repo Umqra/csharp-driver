44f7596d914b2060a532d56034cc0068b978f488
KONTUR\sivukhin <sivukhin@skbkontur.ru>
2019-01-06 12:54:01 +0500

    Introduce simple metrics abstractions inside the driver

---
+++ src/Cassandra/MetricsInfrastructure/Abstractions/DriverMetricsExtensions.cs
@@ -0,0 +1,49 @@
+// 
+//       Copyright (C) 2019 DataStax Inc.
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//       http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+// 
+
+using System;
+
+namespace Cassandra.MetricsInfrastructure.Abstractions
+{
+    public static class DriverMetricsExtensions
+    {
+        public static void Measure(this IDriverTimer driverTimer, Action action)
+        {
+            driverTimer.StartRecording();
+            try
+            {
+                action();
+            }
+            finally
+            {
+                driverTimer.StopRecording();
+            }
+        }
+
+        public static T Measure<T>(this IDriverTimer driverTimer, Func<T> func)
+        {
+            driverTimer.StartRecording();
+            try
+            {
+                return func();
+            }
+            finally
+            {
+                driverTimer.StopRecording();
+            }
+        }
+    }
+}
