842aec2f0a3836125b51f0418f7f5e71d202bad7
Sivukhin Nikita <sivukhin.nikita@yandex.ru>
2019-04-14 16:13:55 +0500

    Remove global prefix options and tune CassandraDriverGraphitePointTextWriter a little instead
    - Now custom text writer can be configured to accept intrinsics global metric tags, s.t. app, env, server

--- src/Cassandra/Metrics/AppMetricsImpl/CassandraDriverGraphitePointTextWriter.cs
+++ src/Cassandra/Metrics/AppMetricsImpl/CassandraDriverGraphitePointTextWriter.cs
@@ -1,17 +1,26 @@
 #if NETSTANDARD2_0
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using App.Metrics;
 using App.Metrics.Formatters.Graphite;
 using App.Metrics.Formatters.Graphite.Internal;
 
 namespace Cassandra.Metrics.AppMetricsImpl
 {
-    /// <summary>
-    /// Simple GraphitePointTextWriter for the App.Metrics
-    /// Ignores all tags and simply write path ${Context.Measurement.Fields} 
-    /// </summary>
     public class CassandraDriverGraphitePointTextWriter : IGraphitePointTextWriter
     {
+        private readonly bool _includeTagName;
+        private readonly string[] _intrinsicTagsToProcess;
+        private static readonly HashSet<string> ExcludeTags = new HashSet<string> {"app", "env", "server", "mtype", "unit", "unit_rate", "unit_dur"};
+
+        public CassandraDriverGraphitePointTextWriter(bool includeTagName, params string[] intrinsicTagsToProcess)
+        {
+            _includeTagName = includeTagName;
+            _intrinsicTagsToProcess = intrinsicTagsToProcess;
+        }
+
         public void Write(TextWriter textWriter, GraphitePoint point, bool writeTimestamp = true)
         {
             if (textWriter == null)
@@ -19,17 +28,7 @@ namespace Cassandra.Metrics.AppMetricsImpl
                 throw new ArgumentNullException(nameof(textWriter));
             }
 
-            var measurementWriter = new StringWriter();
-
-            if (!string.IsNullOrWhiteSpace(point.Context))
-            {
-                measurementWriter.Write(GraphiteSyntax.EscapeName(point.Context, true));
-                measurementWriter.Write(".");
-            }
-
-            measurementWriter.Write(GraphiteSyntax.EscapeName(point.Measurement, true));
-            measurementWriter.Write('.');
-            var prefix = measurementWriter.ToString();
+            var prefix = BuildMetricPath(point);
             var utcTimestamp = point.UtcTimestamp ?? DateTime.UtcNow;
             foreach (var f in point.Fields)
             {
@@ -44,6 +43,39 @@ namespace Cassandra.Metrics.AppMetricsImpl
 
                 textWriter.Write('\n');
             }
+        }
+
+        private string BuildMetricPath(GraphitePoint point)
+        {
+            var metricPath = new List<string>();
+            var tagsDictionary = point.Tags.ToDictionary();
+            foreach (var tag in _intrinsicTagsToProcess)
+            {
+                if (!tagsDictionary.TryGetValue(tag, out var tagValue))
+                    continue;
+                if (_includeTagName)
+                {
+                    metricPath.Add(tag);
+                }
+
+                metricPath.Add(tagValue);
+            }
+
+            if (!string.IsNullOrWhiteSpace(point.Context))
+            {
+                metricPath.Add(GraphiteSyntax.EscapeName(point.Context, true));
+            }
+
+            metricPath.Add(GraphiteSyntax.EscapeName(point.Measurement, true));
+
+            foreach (var tag in tagsDictionary.Where(tag => !ExcludeTags.Contains(tag.Key)))
+            {
+                metricPath.Add(GraphiteSyntax.EscapeName(tag.Key));
+                metricPath.Add(GraphiteSyntax.EscapeName(tag.Value));
+            }
+
+            var prefix = string.Join(".", metricPath);
+            return prefix;
         }
     }
 }
