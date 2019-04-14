1794cdadb60f43a697f30ef610fcd6433ae686ab
Sivukhin Nikita <sivukhin.nikita@yandex.ru>
2019-04-14 11:57:36 +0500

    Collect metrics about bytes transferred over network

--- src/Cassandra/Connection.cs
+++ src/Cassandra/Connection.cs
@@ -489,6 +489,8 @@ namespace Cassandra
                 //All pending operations have been canceled, there is no point in reading from the wire.
                 return;
             }
+            
+            _connectionLevelMetricsRegistry.BytesReceived.Increment(bytesReceived);
             //We are currently using an IO Thread
             //Parse the data received
             var streamIdAvailable = ReadParse(buffer, bytesReceived);
@@ -827,7 +829,9 @@ namespace Cassandra
                 }
                 //We will not use the request any more, stop reference it.
                 state.Request = null;
+                // todo(sivukhin, 14.04.2019): Refactor this part a little?
                 totalLength += frameLength;
+                _connectionLevelMetricsRegistry.BytesSent.Increment(frameLength);
             }
             if (totalLength == 0L)
             {
